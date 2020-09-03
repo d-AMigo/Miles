8fc784bf9f84915c94fa027a80ede254d805f311
Dmitriy Migorenko <migorenko.dmitry@yandex.ru>
2020-09-03 21:22:47 +0300

    project added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int freeMiles = ticketPrice / 20;
+        System.out.println(freeMiles);
+    }
+}
